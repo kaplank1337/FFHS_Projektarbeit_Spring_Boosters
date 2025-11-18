c073a7e6d53b0db26810e9ba099d9d10a51c03f0
Kaan Kaplan <kaan.kaplan@sbb.ch>
2025-11-18 14:55:31 +0100

    -ImmunizationPlan fertig getestet!

--- core_backend/src/main/java/ch/ffhs/spring_boosters/controller/dto/VaccineTypeDto.java
+++ core_backend/src/main/java/ch/ffhs/spring_boosters/controller/dto/VaccineTypeDto.java
@@ -3,8 +3,10 @@ package ch.ffhs.spring_boosters.controller.dto;
 import ch.ffhs.spring_boosters.controller.entity.VaccineTypeActiveSubstance;
 
 import java.util.List;
+import java.util.UUID;
 
 public record VaccineTypeDto(
+        UUID id,
         String name,
         String code,
         List<VaccineTypeActiveSubstance> vaccineTypeActiveSubstances
